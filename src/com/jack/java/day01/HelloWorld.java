407fcb81b71fd9cdf9d5de704ba60fb1d1ebdcc3
金小贱 <786518239@qq.com>
2022-08-04 17:52:21 +0800

    java project 学习 java

---
+++ src/com/jack/java/day01/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.jack.java.day01;
+
+/** java 第一个 helloword 程序
+ * @author jack
+ */
+
+public class HelloWorld {
+    //类的声明
+    //代码必须写在类体内
+    public static void main(String[] args) {
+        //定义一个变量a，给一个初始值1
+        int a = 1;
+        //定义一个变量b，给一个初始值2
+        int b = 2;
+        //输出语句
+        System.out.println(a + b);
+    }
+}
---
+++ src/com/jack/java/day02/VariableLearning.java
@@ -0,0 +1,8 @@
+package com.jack.java.day02;
+
+public class VariableLearning {
+    public static void main(String[] args) {
+        //定义布尔类型的变量
+        boolean aa = true;
+    }
+}
