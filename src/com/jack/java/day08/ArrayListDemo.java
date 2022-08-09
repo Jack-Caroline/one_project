ecd3a00f2b4201c7dc0b82b431331f48d2718d70
jack <786518239@qq.com>
2022-08-09 23:56:49 +0800

    第八天：集合ArrayList

---
+++ src/com/jack/java/day08/ArrayListDemo.java
@@ -0,0 +1,9 @@
+package com.jack.java.day08;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+    }
+}
